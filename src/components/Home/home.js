9ac0c5167f4365a75886a13594e00834c8038481
Jens Tuyls <you@example.com>
2018-09-15 15:52:47 -0400

    added reactstrap and did initial UI design

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import './home.css';
 
 import Signup from '../Signup/signup';
 
 class Home extends Component {
   render() {
     return (
-      <Signup></Signup>
+      <div className="container-fluid home-container">
+        <h1>PonderPal</h1>
+        <Signup></Signup>
+      </div>
     );
   }
 }
