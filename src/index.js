bf3c0565f548fbbcf342d8ed8cd7e9946b2580e6
Jens Tuyls <you@example.com>
2018-09-15 10:03:22 -0400

    added redux setup

--- src/index.js
+++ src/index.js
@@ -4,5 +4,16 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk'
+import reducers from './reducers'
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+document.getElementById('root'));
 registerServiceWorker();
