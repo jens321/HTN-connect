1edc0d5a038b96d1dd784c149865cc0998a20791
Narayan Sub <nanujps@gmail.com>
2018-09-15 20:09:10 -0400

    added 3 input fields

--- src/components/QuestionForm/question.js
+++ src/components/QuestionForm/question.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import Slider from 'react-rangeslider'
 import './question.css';
 
 class Question extends Component {
@@ -7,13 +8,47 @@ class Question extends Component {
     super(props)
 
     this.state = {
-      // add as many attributes here to store the answers for the question
+    country:"",
+    languages:"",
+    neighbourhood:"",
     }
   }
 
   render() {
     return(
-      <h2>test</h2>
+      <Form>
+        <FormGroup>
+        <Label for="languages">
+          What language are you most comfortable communicating in (text AND voice)?</Label>
+          <Input type="select" name="languages" id="languages">
+          <option>English</option>
+          <option>French</option>
+          <option>Flemish</option>
+          <option>Korean</option>
+          </Input>
+        </FormGroup>
+
+        <FormGroup>
+          <Label for="country">
+          Which country did you mainly spend your childhood (ages 3-13) in?</Label>
+          <Input type="select" name="country" id="country">
+          <option>Belgium</option>
+          <option>Canada</option>
+          <option>United States of America</option>
+          <option>China</option>
+          </Input>
+        </FormGroup>
+        <FormGroup>
+        <Label for="neighbourhood">
+          What type of region was that in?</Label>
+          <Input type="select" name="neighbourhood" id="neighbourhood">
+          <option>Suburbs</option>
+          <option>City</option>
+          <option>Pretty isolated</option>
+          <option>A mix of everything</option>
+          </Input>
+        </FormGroup>
+      </Form>
     );
   }
 }
