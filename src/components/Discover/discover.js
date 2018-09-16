80b87d7247b8ad4cac90d0b5428608261fae1c47
Andrew <andrewshin8@gmail.com>
2018-09-15 21:57:38 -0700

    basic structure for discover tab, except variables

--- src/components/Discover/discover.js
+++ src/components/Discover/discover.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, FormText, Container, Row, Col } from 'reactstrap';
 import './discover.css';
 
 class Discover extends Component {
@@ -16,10 +16,56 @@ class Discover extends Component {
   render() {
     return(
       <div>
-        <FormGroup>
-          <Label className="profile" for="textOutput">Profile</Label>
-          <Input type="textarea" name="text" id="textOutput" />
-        </FormGroup>
+        <Container>
+          <Row className='discoverButton'>
+            <Col md={{ size: 1, offset: 4 }}> 
+              <Button id="skipButton" outline color="danger">Skip</Button>{''}
+            </Col>
+            <Col md={{ size: 1, offset: 1 }}>
+              <Button id="connectButton" outline color="primary">Connect</Button>
+            </Col>
+          </Row>
+
+        
+          <Row className='discoverRow'>
+            <Col>
+              Country: 
+            </Col>
+            <Col>
+              Language:
+            </Col>
+        
+          </Row>
+          <Row className='discoverRow'>
+            <Col>
+              Region:
+            </Col>
+            <Col>
+              Age:
+            </Col>
+          </Row>
+
+          <Row className='discoverRow'>
+            <Col>
+              Political Spectrum:
+            </Col>
+            <Col>
+              Gender:
+            </Col>
+          </Row>
+
+          <Row className='discoverRow'>
+            <Col>
+              Socioeconomic Group:
+            </Col>
+            <Col>
+              Religion:
+            </Col>
+          </Row>
+          
+        </Container>
+        
+
       </div>
     );
   }
